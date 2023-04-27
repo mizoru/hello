362905758504a2c1c5d2f292d1126cfa00cb0ba1
mizoru <76188535+mizoru@users.noreply.github.com>
2023-04-27 18:39:00 +0300

    calculator

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    num1 = int(request.form['num1'])
+    num2 = int(request.form['num2'])
+    operation = request.form['operation']
+    if operation == 'add':
+        result = num1 + num2
+    elif operation == 'subtract':
+        result = num1 - num2
+    elif operation == 'multiply':
+        result = num1 * num2
+    elif operation == 'divide':
+        result = num1 / num2
+    return render_template('result.html', result=result)
